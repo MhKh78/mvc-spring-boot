01b919847e5e414726dc21c71393c23e485963a9
MhKh78 <38617736+MhKh78@users.noreply.github.com>
2020-03-19 03:07:22 +0330

    faced Tomcat Jsp Issue

---
+++ src/main/java/academy/learnprogramming/bootdemo/controller/DemoController.java
@@ -0,0 +1,28 @@
+package academy.learnprogramming.bootdemo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+public class DemoController {
+
+    //    http://localhost:3000/demo
+    @ResponseBody
+    @GetMapping("demo")
+    public String demo(){
+        log.info("demo method called");
+        return "Hello Spring Boot";
+    }
+
+//    http://localhost:3000/welcome
+    @GetMapping("welcome")
+    public String welcome(Model model){
+        log.info("welcome method called");
+        model.addAttribute("message", "Welcome to Spring Boot");
+        return "welcome";
+    }
+}
---
+++ src/test/java/academy/learnprogramming/bootdemo/BootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming.bootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
